9a8207ad4f8f8e7e89a47155fde3e93eecffd9f4
maxyou <hyyou2010@gmail.com>
2020-08-03 13:40:55 +0800

    similarTwoParts

--- src/main/kotlin/SimilarTwoParts/Solution.kt
+++ src/main/kotlin/SimilarTwoParts/Solution.kt
@@ -2,14 +2,33 @@ package SimilarTwoParts
 
 import TreeNode
 
-
 class Solution {
-//    class TreeNode(var `val`: Int) {
-//             var left: TreeNode? = null
-//             var right: TreeNode? = null
-//         }
     fun similarTwoParts(root: TreeNode?): Int {
 
-        return 0
+        val nodeSumList = mutableListOf<Pair<TreeNode, Int>>()
+
+        fun getTreeSum(tn:TreeNode?):Int{
+            if(tn == null){
+                return 0
+            }
+            val sum = tn.`val` + getTreeSum(tn.left) + getTreeSum(tn.right)
+            nodeSumList.add(Pair(tn, sum))
+            return sum
+        }
+
+        val rootSum = getTreeSum(root)
+
+        var v:Any? = null
+
+        for(i in 0 until nodeSumList.size - 1){ //最后一个是头节点
+            var vv = Math.abs(rootSum - nodeSumList[i].second * 2)
+            if(v == null){
+                v = vv
+            }else{
+                v = Math.min(v as Int, vv)
+            }
+            println(v)
+        }
+        return v as Int
     }
 }
--- src/test/kotlin/SimilarTwoParts/SolutionTest.kt
+++ src/test/kotlin/SimilarTwoParts/SolutionTest.kt
@@ -5,13 +5,10 @@ import anyArray2TreeNode
 import org.junit.Test
 
 class SolutionTest {
-    @Test
-    fun solutionTest() {
-
-        var a = anyArray2TreeNode(arrayOf(1, 3, 5, null, 7, 9))
-        println(a)
-//        val result = Solution().similarTwoParts( )
-//        println(result.toString())
-        println("=======similarTwoParts==========")
-    }
+//    @Test
+//    fun solutionTest() {
+//        var a = anyArray2TreeNode(arrayOf(3,-1))
+//        val result = Solution().similarTwoParts( a )
+//        println("=======similarTwoParts==========")
+//    }
 }
